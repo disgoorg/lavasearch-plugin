12547850ae07d1796463bc038bf992b07383179e
Toπ <git@topi.wtf>
2023-08-12 23:11:04 +0200

    update to latest lavasearch changes

--- go.mod
+++ go.mod
@@ -2,9 +2,13 @@ module github.com/disgoorg/lavasearch-plugin
 
 go 1.20
 
-require github.com/disgoorg/disgolink/v3 v3.0.0-20230724011028-c7c5a2ba8df9
+require (
+	github.com/disgoorg/disgolink/v3 v3.0.0-20230810103102-c572e085fba1
+	github.com/disgoorg/json v1.1.0
+)
 
 require (
-	github.com/disgoorg/json v1.1.0 // indirect
+	github.com/disgoorg/log v1.2.1 // indirect
 	github.com/disgoorg/snowflake/v2 v2.0.1 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
 )
--- types.go
+++ types.go
@@ -15,49 +15,14 @@ const (
 )
 
 type SearchResult struct {
-	Albums    []Album    `json:"albums"`
-	Artists   []Artist   `json:"artists"`
-	Playlists []Playlist `json:"playlists"`
-	Tracks    []Track    `json:"tracks"`
-	Texts     []Text     `json:"texts"`
-}
-
-type Album struct {
-	ID         string  `json:"identifier"`
-	Name       string  `json:"name"`
-	Artist     string  `json:"artist"`
-	URL        string  `json:"url"`
-	TrackCount int     `json:"trackCount"`
-	ArtworkURL *string `json:"artworkUrl"`
-	ISRC       *string `json:"isrc"`
-}
-
-type Artist struct {
-	ID         string  `json:"identifier"`
-	Name       string  `json:"name"`
-	URL        string  `json:"url"`
-	ArtworkURL *string `json:"artworkUrl"`
-}
-
-type Playlist struct {
-	ID         string  `json:"identifier"`
-	Name       string  `json:"name"`
-	URL        string  `json:"url"`
-	TrackCount int     `json:"trackCount"`
-	ArtworkURL *string `json:"artworkUrl"`
-}
-
-type Track struct {
-	Title      string            `json:"title"`
-	Author     string            `json:"author"`
-	Length     lavalink.Duration `json:"length"`
-	ID         string            `json:"identifier"`
-	IsStream   bool              `json:"isStream"`
-	URI        string            `json:"uri"`
-	ArtworkURL *string           `json:"artworkUrl"`
-	ISRC       *string           `json:"isrc"`
+	Tracks    []lavalink.Track    `json:"tracks"`
+	Albums    []lavalink.Playlist `json:"albums"`
+	Artists   []lavalink.Playlist `json:"artists"`
+	Playlists []lavalink.Playlist `json:"playlists"`
+	Texts     []Text              `json:"texts"`
 }
 
 type Text struct {
-	Text string `json:"text"`
+	Text   string              `json:"text"`
+	Plugin lavalink.PluginInfo `json:"pluginInfo"`
 }
